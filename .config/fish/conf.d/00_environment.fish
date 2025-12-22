2bbf4ad3f6b896c48164e320700337cb9b6e7fa3
Joe Halliwell <joe.halliwell@gmail.com>
2025-12-22 12:04:15 +0000

    Rationalize environment further

--- .config/fish/conf.d/00_environment.fish
+++ .config/fish/conf.d/00_environment.fish
@@ -1,9 +1,9 @@
 if status is-interactive
     for line in (systemctl --user show-environment)
         set -l kv (string split -m 1 = -- $line)
-        
+
         # Guard clause: skip empty values or protected variables
-        if test -z "$kv[2]"; or contains -- $kv[1] PWD SHLVL _
+        if test -z "$kv[2]"; or contains -- $kv[1] PATH PWD SHELL SHLVL _
             continue
         end
 
---
+++ .config/fish/conf.d/10_bat.fish
@@ -0,0 +1,18 @@
+if type -q bat
+    # Use 'abbr' so it expands in-place (active freedom)
+    abbr -a cat bat
+
+    # Theme and Manpage integration
+    set -gx BAT_THEME "TwoDark"
+    set -gx MANPAGER "sh -c 'col -bx | bat -l man -p'"
+    set -gx MANROFFOPT "-c"
+
+    # File preview for fzf
+    set -gx FZF_DEFAULT_OPTS "--preview 'bat --style=numbers --color=always --line-range :500 {}'"
+
+    # Colorize --help messages
+    # Usage: helpme <command> (e.g., helpme git)
+    function helpme
+        $argv --help 2>&1 | bat --plain --language=help
+    end
+end
