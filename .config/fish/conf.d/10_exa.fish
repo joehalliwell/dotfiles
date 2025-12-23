242d4cdf577d9b33301b55c94abbab73897d325a
Joe Halliwell <joe.halliwell@gmail.com>
2025-12-23 11:14:08 +0000

    Add fish plugins (fisher, fzf, zoxide)

---
+++ .config/fish/conf.d/10_exa.fish
@@ -0,0 +1,7 @@
+if type -q eza
+    # -l: long, --icons: visual, --git: status, -a: all
+    abbr -a ll 'eza --classify --long --icons --git --all'
+    abbr -a ls 'eza --classify'
+    abbr -a tree 'eza --tree --icons'
+end
+
