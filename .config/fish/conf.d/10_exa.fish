6a7db4a15c52a44e4bcfa435d2c1e667662c5cbf
Joe Halliwell <joe.halliwell@gmail.com>
2026-01-02 10:15:48 +0000

    Group directories first in ls/eza

--- .config/fish/conf.d/10_exa.fish
+++ .config/fish/conf.d/10_exa.fish
@@ -1,7 +1,6 @@
 if type -q eza
     # -l: long, --icons: visual, --git: status, -a: all
-    abbr -a ll 'eza --classify --long --icons --git --all'
-    abbr -a ls 'eza --classify'
+    abbr -a ll 'eza --classify --group-directories-first --long --icons --git --all'
+    abbr -a ls 'eza --classify --group-directories-first'
     abbr -a tree 'eza --tree --icons'
 end
-
